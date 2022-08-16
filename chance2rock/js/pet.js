dffcd25b2441066f4510113b56ce04dcbf2279ff
Fig <figueroa.jorge20@gmail.com>
2022-08-16 19:38:32 -0400

    update made for pet.html to see if image with text overlay is an option

--- chance2rock/js/pet.js
+++ chance2rock/js/pet.js
@@ -5,13 +5,13 @@ function createCard([img,product,nameText,birthDate,weight,height,coatColor,nose
     <div class="galleryCard">
     <img src="${img}" alt="${product}" class="card-img-top embed-responsive-21by9">
         <div class="cardText">
-            <p class="nameText">${nameText}</p>
-            <h2 class="birthDate">${birthDate}</h2>
-            <h2 class="weight">Weight:${weight}</h2>
-            <h5>⭐⭐⭐⭐⭐</h5>
+            <h2 class="nameText">${nameText}</h2>
+            <h5 class="birthDate">${birthDate}</h5>
+            <h5 class="weight">Weight:${weight}</h5>
             <p class="height">Height:${height}</p>
             <p class="coatColor"><span>Coat Color:</span>${coatColor}</p>
             <p class="noseColor"><span>Nose Color:</span>${noseColor}</p>
+            <h5>⭐⭐⭐⭐⭐</h5>
         </div>
     </div>
     `;
@@ -19,6 +19,7 @@ function createCard([img,product,nameText,birthDate,weight,height,coatColor,nose
 }
 
 let item1 =["https://fig369.github.io/chance2rock/images/g-2.jpeg",
+"American Bullies XL",
 "Incredibullz Radio AKA Cuba",
 "July 7th 2020",
 "105 lbs",
@@ -28,6 +29,7 @@ let item1 =["https://fig369.github.io/chance2rock/images/g-2.jpeg",
 ];
 
 let item2 =["https://fig369.github.io/chance2rock/images/g-4.jpeg",
+"American Bullies XL",
 "Dog 2",
 "April 20th 2020",
 "105 lbs",
@@ -37,6 +39,7 @@ let item2 =["https://fig369.github.io/chance2rock/images/g-4.jpeg",
 ];
 
 let item3 =["https://images.unsplash.com/photo-1513201099705-a9746e1e201f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+"American Bullies XL",
 "TBD",
 "Fall 2022",
 "TBD",
